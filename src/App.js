aeb78c3c811bdec4237e6966d940385390f52af9
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2025-12-03 22:30:20 +0530

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App built by docker and deployed using github actions in elasticbeanstalk.
         </p>
         <a
           className="App-link"
